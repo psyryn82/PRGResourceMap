3423007fe5659682e13d7a0e56317d9830996c13
Psyryn <dwethered@outlook.com>
2023-12-16 18:34:16 -0800

    Atlas Resource Map

---
+++ json/config.js
@@ -0,0 +1,27 @@
+{
+	"AtlasMapServer": false,
+	"GPSBounds": {
+		"max": [
+			100,
+			-100
+		],
+		"min": [
+			-100,
+			100
+		]
+	},
+	"GridOffset": 23333.333333333332,
+	"GridSize": 1400000,
+	"ItemLink": true,
+	"KofiLink": false,
+	"NodesPerAxis": 30,
+	"PathFinder": true,
+	"PinTool": true,
+	"ServersX": 5,
+	"ServersY": 5,
+	"WarehouseTool": false,
+	"XRange": 200,
+	"XScale": 1,
+	"YRange": 200,
+	"YScale": 1
+}
